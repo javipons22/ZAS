e72b1e609d4d0432c2b639c3d1627a2daa7e5ca9
Javier Pons <javipons@mail.com>
2021-08-17 20:41:09 -0300

    finished section cobertura

--- js/splide.js
+++ js/splide.js
@@ -1,11 +1,14 @@
 new Splide( '#splide-marcas', {
 	type   : 'loop',
-	perPage: 6,
+	perPage: 8,
 	arrows: false,
 	breakpoints: {
 		767: {
 			perPage: 3,
 			
 		},
+		991: {
+			perPage: 6
+		}
 	}
 } ).mount();
